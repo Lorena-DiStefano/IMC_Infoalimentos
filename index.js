916488aae45498195a4eacd41c9c933fb7ea4164
Lorena Di Stefano <lore.distefano@gmail.com>
2022-09-08 20:58:52 -0300

    incorporando Arrays

--- index.js
+++ index.js
@@ -2,63 +2,91 @@ let nombre = prompt("Por favor, ingresa tu nombre")
 const SALUDO = "¡HOLA " + (nombre).toUpperCase() + "! \nPara poder calcular tu IMC necesitamos conocer tu ALTURA y PESO"
 alert(SALUDO)
 
-function calculoImc() {
+function calculador() {
     let altura = parseFloat(prompt("Ingresa tu altura expresada en metros" + "\nEjemplo:1.64"))
     let peso = parseFloat(prompt("Ingresa tu peso expresado en kg" + "\nEjemplo:75.20"))
 
-    const ALT = ((altura) * (altura))
-    const IMC = ((peso) / ALT).toFixed(2)
-    const MIN = (ALT * 18.5)
-    const MAX = (ALT * 25)
+    const ALT = Math.pow(altura, 2)
+    const IMC = (peso/ALT).toFixed(2)
 
-    if (IMC <= 16) {
-        alert("Tu IMC es " + IMC + ' "Delgadez Extrema" ')
-    } else
-        if (IMC > 16 && IMC <= 17) {
-            alert("Tu IMC es " + IMC + ' e indica: "Delgadez Moderada" ')
-        } else
-            if (IMC > 17 && IMC <= 18.5) {
-                alert("Tu IMC es " + IMC + ' e indica: "Delgadez Leve" ')
-            } else
-                if (IMC > 18.5 && IMC <= 25) {
-                    alert("Tu IMC es " + IMC + ' e indica: "Normal" ')
-                } else
-                    if (IMC > 25 && IMC <= 30) {
-                        alert("Tu IMC es " + IMC + " e indica: 'Sobrepeso'")
-                    } else
-                        if (IMC > 30 && IMC <= 35) {
-                            alert("Tu IMC es " + IMC + ' e indica: "Obesidad Leve" ')
-                        } else
-                            if (IMC > 35 && IMC <= 40) {
-                                alert("Tu IMC es " + IMC + ' e indica: "Obesidad Moderada" ')
-                            } else
-                                if (IMC > 40) {
-                                    alert("Tu IMC es " + IMC + ' e indica: "Obesidad Morbida" ')
-                                }
+    function calculaPesoNormal(ALT, valor1) {
+        const pesoNormal = (ALT * valor1).toFixed(2)
+        return pesoNormal
+    }
+
+
+
+
+    let diagnostico = [
+        "Delgadez Extrema",
+        "Delgadez Moderada",
+        "Delgadez Leve",
+        "Peso Normal",
+        "Sobrepeso",
+        "Obesidad leve",
+        "Obesidad Moderada",
+        "Obesidad Mórbida"
+    ]
+
+    //Operador condicional Ternario
+    const TYPE =
+        IMC <= 16 ? 0 :
+            IMC >= 16 && IMC < 17 ? 1 :
+                IMC >= 17 && IMC < 18.5 ? 2 :
+                    IMC >= 18.5 && IMC < 25 ? 3 :
+                        IMC >= 25 && IMC < 30 ? 4 :
+                            IMC >= 30 && IMC < 35 ? 5 :
+                                IMC >= 35 && IMC < 40 ? 6 :
+                                    IMC >= 40 ? 7 : null
+
+    alert(`Tu IMC es de ${IMC} e indica ${diagnostico[TYPE]}`)
+
+    let menu = [
+        "Nuevo cálculo",
+        "Mi rango de peso Normal",
+        "Qué debo saber sobre el índice de Quetelet? "
+    ]
+
+    function crearMenu() {
+        let mensaje = "Menú de opciones"
+        let count = 1
+
+        for (let item of menu) {
+            mensaje += `\n${count}. ${item}`
+            count++
+        }
+        mensaje += `\n${count}. Finalizar`
+        return mensaje
+    }
+
     let opciones
+    const calculationArray=[]
+    
+
     do {
-        opciones = parseInt(prompt("Elije una opción \n 1. Volver a realizar el cálculo \n 2. ¿Cual es tu rango de peso normal? \n 3. Más Información \n 4. Finalizar"))
+        opciones = parseInt(prompt(crearMenu()))
 
         switch (opciones) {
             case 1:
-                calculoImc()
+                calculador()
                 opciones = 4
                 break
             case 2:
-                alert("Tu peso normal está entre " + MIN.toFixed(2) + " y " + MAX.toFixed(2))
+                alert(`Tu peso normal es entre ${calculaPesoNormal(ALT, 18.5)}kg. y ${calculaPesoNormal(ALT, 25)}kg.`)
                 break
             case 3:
                 alert("IMPORTANTE: La tabla o índice de Quetelet, fue creada en 1832. \nNo contempla edad, género, porcentaje de grasa corporal o masa muscular. \nConsiderando que el criterio sobre 'la belleza de los cuerpos' varía según la época y las culturas; te propongo que hablemos de 'SALUD'.\nUna 'BUENA SALUD' no es una cuestión de 'ESTETICA', consiste en realizar chequeos periódicos, los cuales te porporcionarán resultados certeros sobre lo que tu organismo necesita.")
                 break
             case 4:
                 alert("Gracias por visitar nuestra página")
+                console.log(calculationArray)
                 break
         }
 
     } while (opciones !== 4)
 }
 
-calculoImc()
+calculador()
 
 
 
